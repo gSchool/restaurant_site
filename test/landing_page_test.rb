08104b981d31f6ca4cf8acccbf4721532bc81bd0
Kinsey Durham <kinseyann505@gmail.com>
2014-01-22 10:29:29 -0700

    A list of 5 dishes displays on the page

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -17,7 +17,6 @@ describe "Landing page" do
   end
 
   it "displays two paragraphs of welcome text" do
-    skip
     welcome_text = page.all('html body main p')
     welcome_text.length.must_equal 2
 
@@ -27,7 +26,6 @@ describe "Landing page" do
   end
 
   it "displays a list of names for 5 dishes" do
-    skip
     dishes = page.all('html body main ul li')
     dishes.length.must_equal 5
 
